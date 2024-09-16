dc5d3c587a60ebb5cdf5dd9a354874541bd5c4bc
Nelman25 <jonelvillaver735@gmail.com>
2024-09-16 16:44:35 +0800

    some updates on ui

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
+import pokemonLogo from "./assets/pokemonLogo.png";
 
 const App = () => {
 	const [searchedPokemon, setSearchedPokemon] = useState("");
@@ -17,7 +18,7 @@ const App = () => {
 	const handleSearchPokemon = async () => {
 		try {
 			const response = await fetch(
-				`https://pokeapi.co/api/v2/pokemon/${searchedPokemon}`
+				`https://pokeapi.co/api/v2/pokemon/${searchedPokemon.toLowerCase()}`
 			);
 			if (!response.ok) {
 				throw new Error(
@@ -35,6 +36,7 @@ const App = () => {
 					: data.sprites.back_default,
 			};
 			setPokemonData(pokemonDataObject);
+			setPokemonIsValid(true);
 		} catch (error) {
 			console.error(error);
 			setPokemonIsValid(false);
@@ -46,34 +48,39 @@ const App = () => {
 	};
 
 	return (
-		<div className="bg-slate-200 text-center h-[800px]">
-			<h1 className="text-3xl bold ">Pokemon</h1>
-			<label htmlFor="searchPokemon" className="text-2xl bold">
-				Search pokemon:{" "}
-			</label>
-
-			<div className="flex justify-center items-center">
-				<Input
-					id="searchPokemon"
-					className="w-1/3"
-					value={searchedPokemon}
-					onChange={handleChangeSearch}
-				/>
-				<Button onClick={handleSearchPokemon} className="">
-					Search
-				</Button>
+		<div className="bg-slate-200 text-center h-screen font-palanquin]">
+			<div className="flex justify-center items-center gap-4">
+				<img src={pokemonLogo} width={200} height={100} />
+				<div className="flex w-[800px] items-center space-x-2">
+					<Input
+						id="searchPokemon"
+						placeholder="Search a pokemon"
+						value={searchedPokemon}
+						onChange={handleChangeSearch}
+					/>
+					<Button onClick={handleSearchPokemon} type="submit">
+						Search
+					</Button>
+				</div>
 			</div>
 
-			<div>
+			<div className="mx-auto p-8 w-[30%] h-[81%] bg-yellow-200 rounded-lg shadow-2xl border border-black rotateY180 perspective">
 				{pokemonIsValid ? (
-					<div className="text-start">
-						<h3>Name: {pokemonData.name} </h3>
-						<p>Height: {pokemonData.height} </p>
-						<p>Weight: {pokemonData.weight} </p>
-						<img src={pokemonData.pokemonImg} alt="image of the pokemon" />
+					<div className="flex flex-col justify-center items-center bg-orange-300">
+						<img
+							src={pokemonData.pokemonImg}
+							className="w-[510px] h-[525px] object-contain border-2 border-black drop-shadow-2xl "
+						/>
+						<div className="px-4 py-8 text-start text-xl border-2 border-black w-full border-t-0">
+							<h3 className="">Name: {pokemonData.name} </h3>
+							<p>Height: {pokemonData.height} </p>
+							<p>Weight: {pokemonData.weight} </p>
+						</div>
 					</div>
 				) : (
-					<p>Pokemon not found, please check spelling and try again.</p>
+					<p className="text-2xl p-24 text-red-600 bold">
+						Pokemon not found, please check spelling and try again.
+					</p>
 				)}
 			</div>
 		</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	darkMode: ["class"],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-    	extend: {
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		},
-    		colors: {}
-    	}
-    },
+		extend: {
+			borderRadius: {
+				lg: "var(--radius)",
+				md: "calc(var(--radius) - 2px)",
+				sm: "calc(var(--radius) - 4px)",
+			},
+			fontFamily: {
+				palanquin: ["MyCustomFont", "sans-serif"],
+			},
+
+			colors: {},
+		},
+	},
 	plugins: [require("tailwindcss-animate")],
 };
